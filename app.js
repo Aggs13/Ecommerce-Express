15aa46e7e2ebbbcc473f58b35820e923337ad637
Aggs13 <agusgualda033@gmail.com>
2026-04-07 01:27:25 -0300

    Agrego la vista Login y Registrarse, cree sus rutas especificas en app.js

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express")
+const path = require("path")
 const app = express()
 
-app.set("view engine", "ejs");
-
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
+console.log(__dirname);
 
 app.listen(3000,()=>{
   console.log("Servidor en http://localhost:3000/")
 })
 
+// Rutas 
 app.get("/", (req,res) => {
-  res.render("Login")
+  res.render("pages/Login.ejs")
+})
+
+
+app.get("/Registrarse",(req,res) => {
+  res.render("pages/Registrar")
 })
