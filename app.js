97c0bafb63932d2e26cef7faca741348048aa5cd
EnzoLlanos <enzollanos16@gmail.com>
2026-04-07 14:30:38 -0300

    prueba

--- app.js
+++ app.js
@@ -6,9 +6,10 @@ app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"));
 console.log(__dirname);
 
-app.listen(3000,()=>{
+app.listen(3000,()=>
   console.log("Servidor en http://localhost:3000/")
-})
+)
+
 
 // Rutas 
 app.get("/", (req,res) => {
@@ -18,4 +19,11 @@ app.get("/", (req,res) => {
 
 app.get("/Registrarse",(req,res) => {
   res.render("pages/Registrar")
+})
+
+app.get("/inicio", (req, res)=>{
+  res.render("pages/Inicio")
+})
+app.get("/inicio", (req, res)=>{
+  res.render("pages/Inicio")
 })
