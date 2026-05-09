dcfddac1483a5f8627da9628e7d4488d2e0333fb
Aggs13 <agusgualda033@gmail.com>
2026-05-09 17:25:02 -0300

    Create Productos.js

---
+++ Data/Productos.js
@@ -0,0 +1,92 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Coca Cola Lata",
+        descripcion: "Bebida gaseosa cola clásica ideal para compartir bien fría.",
+        linkImg:"https://media.istockphoto.com/id/458464735/es/foto/coca-cola.jpg?s=612x612&w=0&k=20&c=SnB7NqAiTxs3PQzWpSpwOiOncP1hbYHEP9zaDurvLwU=",
+        precio: 4200,
+        categoria: "Snacks"
+    },
+    {
+        id: 2,
+        nombre: "Pepsi Black 2L",
+        descripcion: "Gaseosa cola sin azúcar con sabor intenso y refrescante.",
+        linkImg:"",
+        precio: 3900,
+        categoria: "Snacks"
+    },
+    {
+        id: 3,
+        nombre: "Papas Lays Clásicas 150g",
+        descripcion: "Papas fritas crocantes sabor original.",
+        linkImg:"",
+        precio: 2800,
+        categoria: "Snacks"
+    },
+    {
+        id: 4,
+        nombre: "Doritos Queso 140g",
+        descripcion: "Nachos sabor queso con textura súper crocante.",
+        linkImg:"",
+        precio: 3100,
+        categoria: "Snacks"
+    },
+    {
+        id: 5,
+        nombre: "Chocolate Milka Oreo 100g",
+        descripcion: "Chocolate con leche y trozos de galletita Oreo.",
+        linkImg:"",
+        precio: 2200,
+        categoria: "Snacks"
+    },
+    {
+        id: 6,
+        nombre: "Galletitas Oreo 118g",
+        descripcion: "Galletitas de chocolate con relleno sabor vainilla.",
+        linkImg:"",
+        precio: 1700,
+        categoria: "Snacks"
+    },
+    {
+        id: 7,
+        nombre: "Rocklets 40g",
+        descripcion: "Confites de chocolate con cobertura crocante y colores divertidos.",
+        linkImg:"",
+        precio: 1200,
+        categoria: "Snacks"
+    },
+    {
+        id: 8,
+        nombre: "Kinder Bueno",
+        descripcion: "Barra wafer rellena con crema de avellanas y cubierta de chocolate.",
+        linkImg:"",
+        precio: 2500,
+        categoria: "Snacks"
+    },
+    {
+        id: 9,
+        nombre: "Monster Energy 473ml",
+        descripcion: "Bebida energizante sabor original.",
+        linkImg:"",
+        precio: 3600,
+        categoria: "Snacks"
+    },
+    {
+        id: 10,
+        nombre: "Maní Salado 200g",
+        descripcion: "Maní tostado y salado ideal para picadas y snacks.",
+        linkImg:"",
+        precio: 1900,
+        categoria: "Snacks"
+    },
+    {
+        id: 11,
+        nombre: "Palitos Salados 120g",
+        descripcion: "Snack salado crocante perfecto para acompañar dips.",
+        linkImg:"",
+        precio: 1400,
+        categoria: "Snacks"
+    }
+];
+
+module.exports = productos;
