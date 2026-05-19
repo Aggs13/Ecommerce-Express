266e4d3b5318789c7d25f0e71c49671aa80bd53f
EnzoLlanos <enzollanos16@gmail.com>
2026-05-19 14:29:08 -0300

    busquedas
    se puede buscar productos

--- controller/productController.js
+++ controller/productController.js
@@ -23,7 +23,24 @@ function productoDetalles(req,res){
   })
 }
 
+function buscarProductos(req, res){
+  const resultado = producModel.ProductosInicio()
+  const busqueda = req.query.q ? req.query.q.toLowerCase() : ""
+
+  const productos = resultado.productos.filter(p => p.nombre.toLowerCase().includes(busqueda) || p.categoria.toLowerCase().includes(busqueda))
+
+  res.render("Busqueda", {
+    titulo: "Busqueda",
+    page: "inicio",
+    style: "/styles/inicio.css",
+    productos,
+    busqueda
+  })
+
+}
+
 module.exports = {
   mostrarProductosIncio,
-  productoDetalles
+  productoDetalles,
+  buscarProductos
 }
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -4,5 +4,6 @@ const productController = require("../controller/productController")
 
 router.get("/inicio", productController.mostrarProductosIncio)
 router.get("/Detalles/:id", productController.productoDetalles)
+router.get("/buscar", productController.buscarProductos)
 
 module.exports = router
