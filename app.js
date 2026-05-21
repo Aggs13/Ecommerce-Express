2c5a1f1af4d75480b0842b7ed7204379a09a89df
GonzaloBarroso777 <gonzalobarroso25@gmail.com>
2026-05-21 00:11:04 -0300

    pagina de error 500

--- app.js
+++ app.js
@@ -1,116 +1,53 @@
 const express = require("express")
 const path = require("path")
+const session = require("express-session")
+const expressLayouts = require("express-ejs-layouts")
+
+const productosRoute = require("./routes/productRoute")
+const carritoRoute = require("./routes/carritoRoute")
+const usuarioRoute = require("./routes/usuarioRoute")
+const carritoController = require("./controller/CarritoController")
+
 const app = express()
 
 app.set("view engine", "ejs")
-// Carpeta de views
-app.set("views", path.join(__dirname, "views"));
-// Carpeta de public para Stylos o Imagenes
+app.set("views", path.join(__dirname, "views"))
+app.use(expressLayouts)
+app.set("layout", "layouts/main")
+
 app.use(express.static(path.join(__dirname, "public")))
-// Productos
-const productos = require("./Data/Productos")
-// Carrito
-const carrito = require("./Data/Carrito") 
-console.log(__dirname);
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: "ecommerceExpressWeb1",
+  resave: false,
+  saveUninitialized: false
+}))
 
-app.listen(3000,()=>
-  console.log("Servidor en http://localhost:3000/")
-)
-
-
-// Rutas  
-app.get("/", (req,res) => {
-  res.render("pages/Login.ejs")
+app.use((req, res, next) => {
+  res.locals.usuario = req.session.usuario
+  next()
 })
 
+app.use(carritoController.ActualizarContadorCarrito)
 
-app.get("/Registrarse",(req,res) => {
-  res.render("pages/Registrar")
+app.use(productosRoute)
+app.use(carritoRoute)
+app.use(usuarioRoute)
+
+app.use((req, res) => {
+  res.status(404).send("Error 404 - Pagina no encontrada")
 })
 
-app.get("/Detalles/:id",(req,res) => {
-  const id = req.params.id
-  const producto = productos.find(p => p.id == id)
-  const productosSugeridos = productos.filter(p => p.id != id).slice(0,4)
-  res.render("pages/Detalle", {producto, productosSugeridos})
-})
+app.use((err, req, res, next) => {
+  console.error(err)
 
-app.get("/inicio", (req, res)=>{
-  const numeros = [...Array(productos.length).keys()].sort(() => Math.random() - 0.5).slice(0,4)
-
-  console.log(numeros)
-  res.render("pages/Inicio",{productos,numeros})
-})
-
-
-
-app.get("/inicio", (req, res)=>{
-  res.render("pages/Inicio")
-})
-
-
-
-// Ruta para agregar al carrito
-app.get("/agregar-carrito/:id",(req,res)=>{
-  const idp = req.params.id
-  const producto = productos.find(p => p.id == idp)
-  const existe = carrito.find(p => p.id == idp)
-  if(existe){
-    existe.cantidad +=1;
-  }else{
-    const p =  {
-      id : idp,
-      nombre : producto.nombre,
-      precio : producto.precio,
-      img : producto.linkImg,
-      cantidad : 1
-    }
-    carrito.push(p)
-  }
-  console.log(carrito)
-  res.redirect("/Detalles/" + idp)
-})
-
-// Ruta para eliminar del carrito
-app.get("/eliminar-carrito/:id",(req,res)=>{
-  const idp = req.params.id
-  const index = carrito.findIndex(p => p.id == idp)
-  if ( index !==-1) {
-        carrito[index].cantidad--;
-      if(carrito[index].cantidad <= 0){
-        carrito.splice(index,1)
-      }
-    }
-  console.log(carrito)
-  res.redirect("/Carrito")
-})
-//Suamr Carrito
-app.get("/sumar-carrito/:id",(req,res)=>{
-  const idp = req.params.id
-  const producto = productos.find(p => p.id == idp)
-  const existe = carrito.find(p => p.id == idp)
-  if(existe){
-    existe.cantidad +=1;
-  }else{
-    const p =  {
-      id : idp,
-      nombre : producto.nombre,
-      precio : producto.precio,
-      img : producto.linkImg,
-      cantidad : 1
-    }
-    carrito.push(p)
-  }
-  console.log(carrito)
-  res.redirect("/Carrito")
-})
-
-//calcular total
-app.get("/Carrito",(req,res)=>{
-  let total = 0;
-  carrito.forEach(p=> {
-    total += p.precio * p.cantidad
+  res.status(500).render("500", {
+    titulo: "Error 500",
+    page: "",
+    style: "/styles/Error.css"
   })
+})
 
-  res.render("pages/Carrito",{carrito,total})
+app.listen(3000, () => {
+  console.log("Servidor en http://localhost:3000/")
 })
