c047fa012a882c067f5a23f6c030ec9f04020fcd
Aggs13 <agusgualda033@gmail.com>
2026-07-12 17:25:52 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const cors = require("cors")
 const app = express()
 
 // Middlewares generales
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({
+  origin: [
+    "http://localhost:3001",
+    "http://localhost:5173"
+  ],
+  credentials: true
+}));
+
 app.use(express.json())
 
 // Montar rutas bajo /api
